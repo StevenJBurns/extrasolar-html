e11a5d75f743b440de93d31b82fbffbad8538dbe
StevenJBurns <stevenjburns@msn.com>
2022-05-23 10:52:37 -0600

    feat: create temp web components for header and footer

--- src/js/components/header-nav.ts
+++ src/js/components/header-nav.ts
@@ -3,50 +3,48 @@ export class HeaderNav extends HTMLElement {
     super();
   };
 
-  connectedCallback() {
+  async connectedCallback() {
     this.innerHTML = `
-    <style>
-    nav {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      background-color: #0a0a23;
-      height: 4rem;
-      padding: 0.25rem;
-    }
+      <style>
+        nav {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background-color: #0F0F0F;
+          padding: 0.25rem;
+          height: 4rem;
+        }
 
-    ul {
-      display: flex;
-      padding: 0;
-      list-style: none;
-    }
-    
-    a {
-      margin: 0 25px;
-      font-weight: 700;
-      text-decoration: none;
-      color: #FFF;
-    }
-    
-    a:visited {
-      color: #FFF
-    }
+        ul {
+          display: flex;
+          list-style: none;
+          padding: 0;
+        }
+        
+        a {
+          margin: 0 25px;
+          font-weight: 700;
+          text-decoration: none;
+          color: #FFF;
+        }
+        
+        a:visited {
+          color: #FFF
+        }
 
-    a:hover {
-      color: yellow;
-      padding-bottom: 0.5rem;
-    }
-  </style>
+        a:hover {
+          color: yellow;
+          padding-bottom: 0.5rem;
+        }
+      </style>
       <nav>
         <ul>
           <li><a href="/">Home</a></li>
-          <li><a href="data.html">Data</a></li>
-          <li><a href="visualization.html">Visualization</a></li>
-          <li><a href="about.html">About</a></li>
+          <li><a href="/data">Data</a></li>
+          <li><a href="/systems">Systems</a></li>
+          <li><a href="/about">About</a></li>
         </ul>
       </nav>
     `;
   };
 };
-
-// customElements.define('header-nav', HeaderNav);
