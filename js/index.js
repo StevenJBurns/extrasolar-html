e8cd8a3f783b7061e9bc33804527ea7d0b9c0c28
StevenJBurns <stevenjburns@msn.com>
2022-05-24 04:17:17 -0600

    Updates

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 import { handleDOMContentLoaded } from './handleDOMContentLoaded.js';
 import { handleNavigation } from './routing/handleNavigation.js';
 console.log({ location });
-if (!['127.0.0.1', 'localhost'].includes(location.hostname)) {
+if (['127.0.0.1', 'localhost'].includes(location.hostname)) {
     const base = document.createElement('base');
-    base.href = '/extrasolar-html/';
+    base.href = '/';
     document.head.appendChild(base);
 }
 ;
