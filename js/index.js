db31d9c5283e722bd94a595f1127478a1a8e69f7
StevenJBurns <stevenjburns@msn.com>
2022-05-23 23:51:34 -0600

    Updates

--- js/index.js
+++ js/index.js
@@ -1,14 +1,11 @@
-// import { HeaderNav } from './components/header-nav.js';
-// import { SiteFooter } from './components/site-footer.js';
 import { handleDOMContentLoaded } from './handleDOMContentLoaded.js';
 import { handleNavigation } from './routing/handleNavigation.js';
-// customElements.define('header-nav', HeaderNav);
-// customElements.define('site-footer', SiteFooter);
-document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
-window.addEventListener('popstate', handleNavigation);
-if (location.origin !== '127.0.0.1') {
+console.log({ location });
+if (!['127.0.0.1', 'localhost'].includes(location.hostname)) {
     const base = document.createElement('base');
-    base.href = 'href="https://stevenjburns.github.io/extrasolar-html';
+    base.href = 'https://stevenjburns.github.io/extrasolar-html/';
     document.head.appendChild(base);
 }
 ;
+document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
+window.addEventListener('popstate', handleNavigation);
