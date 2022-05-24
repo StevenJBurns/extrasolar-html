978babd3c76e514b5a065570ddb610a54ba3786d
StevenJBurns <stevenjburns@msn.com>
2022-05-24 03:53:58 -0600

    Updates

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 import { handleDOMContentLoaded } from './handleDOMContentLoaded.js';
 import { handleNavigation } from './routing/handleNavigation.js';
 console.log({ location });
-if (!['127.0.0.1', 'localhost'].includes(location.hostname)) {
-    const base = document.createElement('base');
-    base.href = '/extrasolar-html/';
-    document.head.appendChild(base);
-}
-;
+// if (!['127.0.0.1', 'localhost'].includes(location.hostname)) {
+//   const base: HTMLBaseElement = document.createElement('base');
+//   base.href = '/';
+//   document.head.appendChild(base);
+// };
 document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
 window.addEventListener('popstate', handleNavigation);
