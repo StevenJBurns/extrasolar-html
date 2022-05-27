8933de8366960acb90a98bcec7943eaddd54901f
StevenJBurns <stevenjburns@msn.com>
2022-05-27 00:24:42 -0600

    Updates

--- js/routing/getView.js
+++ js/routing/getView.js
@@ -1,5 +1,6 @@
 import { validRoutes } from "./validRoutes.js";
 import views from '../views/index.js';
 export const getView = (path) => {
+    console.log(path);
     return validRoutes.find(route => route.path === path)?.view ?? views.create404View();
 };
