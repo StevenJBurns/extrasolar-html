e917092b6c89d62ed51bda288367bc651be01025
StevenJBurns <stevenjburns@msn.com>
2022-05-30 22:38:23 -0600

    Updates

--- js/index.js
+++ js/index.js
@@ -1,15 +1,8 @@
 import { handleDOMContentLoaded } from './handleDOMContentLoaded.js';
 document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
-const selectDistinctSystems = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+distinct+hostname+from+ps&format=json';
-const fetchOptions = {
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'https://exoplanetarchive.ipac.caltech.edu',
-    },
-};
-const getData = async () => await fetch(selectDistinctSystems, fetchOptions)
-    .then(response => console.log(response.text()))
+// const selectDistinctSystems = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+distinct+hostname+from+ps&format=json';
+const getData = async () => await fetch('https://extrasolar-proxy-server.glitch.me/get')
+    .then(res => res.json())
     .then(data => console.log(data))
     .catch(err => console.error(err));
-console.log(getData());
+getData();
