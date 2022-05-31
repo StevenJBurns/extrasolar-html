8b703c0bfb95be9fb77a527999facce639bb949c
StevenJBurns <stevenjburns@msn.com>
2022-05-31 06:22:43 -0600

    Updates

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 import { handleDOMContentLoaded } from './handleDOMContentLoaded.js';
 document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
-// const selectDistinctSystems = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+distinct+hostname+from+ps&format=json';
 const getData = async () => await fetch('https://extrasolar-proxy-server.glitch.me/get')
     .then(res => res.json())
     .then(data => console.log(data))
